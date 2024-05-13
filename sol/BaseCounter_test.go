0b3a5bf04f1c10b69ca849bcabdbabfca4c58b23
shok1122 <shok1122@zohomail.com>
2024-05-13 22:16:19 +0000

    implement TestBaseCounter

--- sol/BaseCounter_test.go
+++ sol/BaseCounter_test.go
@@ -1,16 +1,54 @@
 package sol
 
 import (
+	"fmt"
 	"testing"
+	"math/big"
 
 	"github.com/stretchr/testify/assert"
 
-	// "github.com/ethereum/go-ethereum/accounts/abi/bind"
-	// "github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
-	// "github.com/ethereum/go-ethereum/core"
-	// "github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
+func setup(t *testing.T) (*backends.SimulatedBackend, *BaseCounter) {
+	t.Helper()
+
+	// generate key
+	key, err := crypto.GenerateKey()
+	assert.NoError(t, err)
+
+	auth, err := bind.NewKeyedTransactorWithChainID(key, big.NewInt(1337))
+	assert.NoError(t, err)
+
+	alloc := core.GenesisAlloc{
+		auth.From: {
+			Balance: big.NewInt(5000000000000000000),
+		},
+	}
+	sim := backends.NewSimulatedBackend(alloc, 10000000)
+	// defer sim.Close()
+
+	// deploy BaseCounter contract
+	_, _, counter, err := DeployBaseCounter(auth, sim, big.NewInt(100))
+	assert.NoError(t, err)
+
+	sim.Commit()
+
+	return sim, counter
+}
+
+
 func TestBaseCounter(t *testing.T) {
-	assert.Equal(t, 0, 0)
+	_, counter := setup(t)
+
+	callOpts := bind.CallOpts{
+		Pending: true,
+	}
+	count, err := counter.Count(&callOpts)
+	assert.NoError(t, err)
+
+	fmt.Println(count)
 }
