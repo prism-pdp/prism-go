e9520825392b57c4de757346fd628597e6af5d63
shok1122 <shok1122@zohomail.com>
2024-07-31 02:46:39 +0000

    Update metadata structure

--- xz21/metadata.go
+++ xz21/metadata.go
@@ -11,22 +11,22 @@ import (
 )
 
 type Metadata struct {
-	Chunk [][]byte // m
-	Hash  [][]byte // M
-	Tag   []*pbc.Element // gamma
+	Size uint32
+	Hash [][]byte
+	Tags []*pbc.Element
 }
 
-func SplitData(_data []byte, _chunkSize int) ([][]byte, error) {
+func SplitData(_data []byte, _chunkSize uint32) ([][]byte, error) {
 	if _chunkSize <= 0 { return nil, fmt.Errorf("invalid split num: %d", _chunkSize) }
 
-	var splitedData [][]byte
-	dataSize := len(_data)
-	rows := int(math.Ceil(float64(dataSize) / float64(_chunkSize)))
+	var chunk [][]byte
+	dataSize := uint32(len(_data))
+	rows := uint32(math.Ceil(float64(dataSize) / float64(_chunkSize)))
 
-	var s, e, processedSize int
+	var s, e, processedSize uint32
 
 	processedSize = 0
-	for i := 0; i < rows; i++ {
+	for i := uint32(0); i < rows; i++ {
 		if i == (rows - 1) {
 			s = i * _chunkSize
 			e = s + (dataSize - processedSize)
@@ -34,48 +34,46 @@ func SplitData(_data []byte, _chunkSize int) ([][]byte, error) {
 			s = i * _chunkSize
 			e = s + _chunkSize
 		}
-		splitedData = append(splitedData, _data[s:e])
+		chunk = append(chunk, _data[s:e])
 		processedSize += (e - s)
 	}
 
-	return splitedData, nil
+	return chunk, nil
 }
 
 // https://github.com/es3ku/z22m2azuma/blob/main/user/src/interfaces/crypt/content.go#L23
-func GenMetadata(_param *PairingParam, _key *PairingKey, _splitedData [][]byte) *Metadata {
-	numChunk := uint32(len(_splitedData))
-
-	// metadata := make([]Metadata, numChunk)
-	var metadata Metadata
-	metadata.Hash = make([][]byte, numChunk)
-	metadata.Chunk = make([][]byte, numChunk)
-	metadata.Tag = make([]*pbc.Element, numChunk)
+func HashChunk(_chunk [][]byte) [][]byte {
+	numChunk := uint32(len(_chunk))
+	hash := make([][]byte, numChunk)
 
 	for i := uint32(0); i < numChunk; i++ {
-		metadata.Hash[i]  = make([]byte, 32)
-		metadata.Chunk[i] = make([]byte, len(_splitedData[i]))
-
-		copy(metadata.Chunk[i], _splitedData[i])
+		hash[i]  = make([]byte, 32)
 	}
 
 	b := make([]byte, 4)
 	for i := uint32(0); i < numChunk; i++ {
 		binary.LittleEndian.PutUint32(b, i)
-		hash := sha256.Sum256(slices.Concat(metadata.Chunk[i], b))
-		copy(metadata.Hash[i], hash[:])
+		h := sha256.Sum256(slices.Concat(_chunk[i], b))
+		copy(hash[i], h[:])
 	}
 
-	metadata.GenTag(_param, _key)
-
-	return &metadata
+	return hash
 }
 
-func (this *Metadata) GenTag(_param *PairingParam, _key *PairingKey) {
-	for i := range this.Tag {
-		e1 := _param.SetFromHash(this.Hash[i])
-		e2 := _param.SetFromHash(this.Chunk[i])
+func GenMetadata(_param *PairingParam, _key *PairingKey, _chunk [][]byte) *Metadata {
+
+	meta := new(Metadata)
+	meta.Size = uint32(len(_chunk))
+	meta.Hash = HashChunk(_chunk)
+	meta.Tags = make([]*pbc.Element, meta.Size)
+
+	for i := uint32(0); i < meta.Size; i++ {
+		e1 := _param.SetFromHash(meta.Hash[i])
+		e2 := _param.SetFromHash(_chunk[i])
 		e3 := _param.PowBig(_param.U, e2.X())
 		e4 := _param.Mul(e1, e3)
-		this.Tag[i] = _param.PowZn(e4, _key.PrivateKey)
+		meta.Tags[i] = _param.PowZn(e4, _key.PrivateKey)
 	}
+
+	return meta
 }
--- xz21/metadata_test.go
+++ xz21/metadata_test.go
@@ -5,16 +5,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSplitFile(t *testing.T) {
-	data := []byte{1,2,3,4,5,6,7,8,9,10,11,12,13}
-	splitedData, err := SplitData(data, 5)
-	assert.NoError(t, err)
-	assert.Equal(t, len(splitedData), 3)
-	assert.Equal(t, len(splitedData[0]), 5)
-	assert.Equal(t, len(splitedData[1]), 5)
-	assert.Equal(t, len(splitedData[2]), 3)
-}
-
 func TestGenMetadata(t *testing.T) {
 	data := []byte{
 		 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
@@ -24,16 +14,18 @@ func TestGenMetadata(t *testing.T) {
 		40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
 		50, 51, 52, 53,
 	}
-	splitedData, err := SplitData(data, 10)
-	assert.NoError(t, err)
 
 	var param PairingParam
 	param.Gen()
 	var key PairingKey
 	key.Gen(&param)
 
-	metadata := GenMetadata(&param, &key, splitedData)
+	chunk, err := SplitData(data, 10)
+	assert.NoError(t, err)
+	assert.Equal(t, len(chunk), 6)
+
+	metadata := GenMetadata(&param, &key, chunk)
+	assert.Equal(t, metadata.Size, uint32(6))
 	assert.Equal(t, len(metadata.Hash), 6)
-	assert.Equal(t, len(metadata.Chunk), 6)
-	assert.Equal(t, len(metadata.Tag), 6)
+	assert.Equal(t, len(metadata.Tags), 6)
 }
