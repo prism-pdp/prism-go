70e20f3891b3387f0fbf62d05bb6756b98cd4caa
shok1122 <shok1122@zohomail.com>
2024-08-09 02:18:44 +0000

    Refactoring

--- xz21/pdp.go
+++ xz21/pdp.go
@@ -91,7 +91,7 @@ func VerifyProof(_param *PairingParam, _tag *Tag, _hashChunks [][]byte, _chal *C
 
 	left  := _param.Pairing.NewG1().Set1()
 	right := _param.Pairing.NewG1().Set1()
-	n := uint32(len(_tag.M))
+	n := uint32(len(_tag.Mu))
 
 	setA := GenA(_chal.K1, _chal.C, n)
 	setV := GenV(_chal.K2, _chal.C, _param)
@@ -101,7 +101,7 @@ func VerifyProof(_param *PairingParam, _tag *Tag, _hashChunks [][]byte, _chal *C
 		v := setV[i]
 
 		// Left
-		t1 := _tag.M[a]
+		t1 := _tag.Mu[a]
 		t2 := _param.PowZn(t1, v)
 		left = _param.Mul(left, t2)
 
--- xz21/tags.go
+++ xz21/tags.go
@@ -10,30 +10,30 @@ import (
 
 type Tag struct {
 	Size uint32
-	M []*pbc.Element
+	Mu []*pbc.Element
 }
 
 type TagData struct {
 	Size uint32
-	M [][]byte
+	Mu [][]byte
 }
 
 func (this *Tag) Export() TagData {
 	var data TagData
-	data.Size = uint32(len(this.M))
-	data.M = make([][]byte, data.Size)
-	for i, v := range this.M {
-		data.M[i] = v.Bytes()
+	data.Size = uint32(len(this.Mu))
+	data.Mu = make([][]byte, data.Size)
+	for i, v := range this.Mu {
+		data.Mu[i] = v.Bytes()
 	}
 	return data
 }
 
 func (this *TagData) Import(_param *PairingParam) Tag {
 	var obj Tag
-	obj.Size = uint32(len(this.M))
-	obj.M = make([]*pbc.Element, obj.Size)
-	for i, v := range this.M {
-		obj.M[i] = _param.Pairing.NewG1().SetBytes(v)
+	obj.Size = uint32(len(this.Mu))
+	obj.Mu = make([]*pbc.Element, obj.Size)
+	for i, v := range this.Mu {
+		obj.Mu[i] = _param.Pairing.NewG1().SetBytes(v)
 	}
 	return obj
 }
@@ -83,14 +83,14 @@ func GenTag(_param *PairingParam, _privKey *pbc.Element, _chunks [][]byte) (Tag,
 
 	var tag Tag
 	tag.Size = uint32(len(_chunks))
-	tag.M = make([]*pbc.Element, tag.Size)
+	tag.Mu = make([]*pbc.Element, tag.Size)
 
 	for i := uint32(0); i < tag.Size; i++ {
 		e1 := _param.SetFromHash(hashChunks[i])
 		e2 := _param.SetFromHash(_chunks[i])
 		e3 := _param.PowBig(_param.U, e2.X())
 		e4 := _param.Mul(e1, e3)
-		tag.M[i] = _param.PowZn(e4, _privKey)
+		tag.Mu[i] = _param.PowZn(e4, _privKey)
 	}
 
 	return tag, hashChunks
